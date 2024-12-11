cfe359d873e095a0174e8d8a0c21b2035bbe74d5
amit kumar rouniyar <58016852+amit-maker@users.noreply.github.com>
2024-12-11 16:33:15 +0545

    Add files via upload

---
+++ easybyte/src/main/java/com/ass/EasybyteApplication.java
@@ -0,0 +1,13 @@
+package com.ass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybyteApplication.class, args);
+	}
+
+}
